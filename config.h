1f1525de52a0fb41fd1107cb273245d8d170559b
QQ <1770362456@qq.com>
2023-02-18 13:24:55 +0800

    增加微信图片查看规则

--- config.h
+++ config.h
@@ -45,7 +45,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ NULL,        NULL,     "图片查看器",       0,             1,           -1 },   //QQ图片查看  
-	{ NULL,        NULL,     "图片查看",         0,             1,           -1 },   //微信图片查看
+	{ NULL,        NULL,     "weixin",           0,             1,           -1 },   //微信图片查看
 	{ "listen1",   NULL,     NULL,               0,             1,           -1 },   //listen1     
 };
 /* layout(s) */
