689c137c556b9cc350a8b49ea32a0b98753c9a3b
QQ <1770362456@qq.com>
2023-02-20 23:33:38 +0800

    集成状态栏

--- dwm.c
+++ dwm.c
@@ -313,17 +313,21 @@ static void open(const Arg *arg);
 static void runcmd(const Arg *arg);
 static void Mythread();
 static void* background(void* arg);
+static void* statusbar(void* arg);
+static const char* RunCMD(const char*);
+static const char* GetDatetime();
 
 
 /* configuration, allows nested code to access above variables */
 #include "config.h"
 
 /* My Functions */
-static pthread_t timing_thread;
+
 void 
 Mythread() 
 {
-  pthread_create(&timing_thread, NULL, background, NULL);
+  static pthread_t timing_thread; pthread_create(&timing_thread, NULL, background, NULL);
+  static pthread_t status_thread; pthread_create(&status_thread, NULL, statusbar, NULL);
 }
 
 void
@@ -345,10 +349,81 @@ open(const Arg *arg)
 void
 Myscripts() 
 {
-  system("QQstatus &");
+  //system("QQstatus &");
   system("fcitx5 -d &");
   system("xfce4-power-manager &");
 }
+
+const char* RunCMD(const char *cmd) {
+  FILE *fp;
+  char res[1024];
+  char *r = res;
+  fp = popen(cmd, "r");
+
+  if (fp == NULL) {
+    perror("popen failed");
+    char *a = "error";
+    r = a;
+  } else  {
+    while (fgets(res, sizeof(res), fp) != NULL) ;
+    char *p = strtok(res, "\n");
+    r = p;
+    pclose(fp);
+  }
+    return r;
+}
+
+const char *GetDatetime() {
+  char res[100];
+  time_t rawtime;
+  struct tm *timeinfo;
+  char buffer[80];
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+
+  strftime(buffer, 80, "%Y-%m-%d %H:%M", timeinfo);
+  sprintf(res, "| %s ", buffer);
+  const char *ans = res;
+  return ans;
+}
+
+void*
+statusbar(void* arg) 
+{
+  static const char *CMDS[] = {
+    "",
+    "uname -r | awk -F '-' '{print $1}'",
+    "amixer sget Master | awk -F '[][]' '/Mono:/{print $2}'",
+    "current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness) && max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness) && brightness_percent=$(( 100 * current_brightness / max_brightness )) && echo $brightness_percent",
+  };
+
+  char command[1024 * 512];
+  while (running) {
+    memset(command, 0, sizeof(command));
+    strcat(command, CMDS[0]);
+    // 第一列
+    strcat(command, "  "); 
+    strcat(command, RunCMD(CMDS[1]));
+    // 第二列
+    strcat(command, "| ");
+    strcat(command, RunCMD(CMDS[2]));
+    // 第三列
+    strcat(command, "| ");
+    strcat(command, RunCMD(CMDS[3]));
+    strcat(command, "%");
+    // 时间
+    strcat(command, GetDatetime());
+
+    char cmd[1024 * 512];
+    sprintf(cmd, "xsetroot -name \"%s\"", command);
+    printf("%s\n", cmd);
+    system(cmd);
+    usleep(100);
+  }
+  return arg;
+}
+
 
 void*
 background(void* arg) 
